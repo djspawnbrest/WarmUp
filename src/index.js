3d1f4fb13aa155d91c5518507b543cfe0412bfd8
Alexander <47751800+djspawnbrest@users.noreply.github.com>
2019-02-18 21:06:46 +0300

    warmup passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  let far = (temperature*9/5)+32; //use standard conversion formula for Celsius to Fahrenheit: Farenheit = (Celsius*9/5)+32
+  return far; // return Farenheit
 };
